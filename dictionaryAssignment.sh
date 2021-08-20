85244bb51e6d1401b10c405dce20b34cacbe03cd
ka1peshc <kalpesh111997@gmail.com>
2021-08-21 02:59:15 +0530

    Solved second question

--- dictionaryAssignment.sh
+++ dictionaryAssignment.sh
@@ -28,3 +28,56 @@ do
 done
 echo "${!rollDice[@]}"
 echo "${rollDice[@]}"
+
+echo "******* question 2 *******"
+declare -A month
+month[Jan]=0
+month[Feb]=0
+month[Mar]=0
+month[Apr]=0
+month[May]=0
+month[Jun]=0
+month[Jul]=0
+month[Aug]=0
+month[Sep]=0
+month[Oct]=0
+month[Nov]=0
+month[Dec]=0
+
+count=0
+while [ $count -lt 50 ]
+do
+    result=$((RANDOM%13))
+    case $result in
+    1)
+        month[Jan]=$((${month[Jan]}+1));;
+    2)
+        month[Feb]=$((${month[Feb]}+1));;
+    3)
+        month[Mar]=$((${month[Mar]}+1));;
+    4)
+        month[Apr]=$((${month[Apr]}+1));;
+    5)
+        month[May]=$((${month[May]}+1));;
+    6)
+        month[Jun]=$((${month[Jun]}+1));;
+    7)
+        month[Jul]=$((${month[Jul]}+1));;
+    8)
+        month[Aug]=$((${month[Aug]}+1));;
+    9)
+        month[Sep]=$((${month[Sep]}+1));;
+    10)
+        month[Oct]=$((${month[Oct]}+1));;
+    11)
+        month[Nov]=$((${month[Nov]}+1));;
+    12)
+        month[Dec]=$((${month[Dec]}+1));;
+    esac
+    ((count++))
+done
+
+for i in ${!month[@]}
+do
+    echo "Month "$i" brithday count "${month[$i]}
+done
